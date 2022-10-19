22fcfab969dccf9e3baffa345885f7779e262c08
Allison Barry <87067546+aliibarry@users.noreply.github.com>
2022-10-19 16:54:21 +0100

    Delete drg-directory_scrap.R

--- drg-directory_scrap.R
+++
@@ -1,8 +0,0 @@
-
-save(file = paste0(PATH_results, "/drg-directory.RData"), 
-     bulkseq_colData, bulkseq_mat, gene_data, PATH_results
-)
-
-PATH_results = "~/AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/amb/Database/drg-directory"
-
-bulkseq_colData <- colData(bulkseq_dds)
