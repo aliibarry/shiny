94b26b1a94f08f74509878246f8b4d4ae730827c
Allison Barry <87067546+aliibarry@users.noreply.github.com>
2022-11-03 15:15:34 +0000

    Add files via upload
    bug correction for superimposed male + female samples when selecting dot plots for "both"

--- server.R
+++ server.R
@@ -5,11 +5,12 @@
 ##   allimariebarry@gmail.com           ##
 ##   for non-commercial use only        ##
 ##########################################
-
 library(profvis)
 # global variables 
-data_dir = "drg-directory.RData"
+data_dir = "/Users/lynnezhao/Desktop/drg/drg-directory.RData"
 load(data_dir)
+
+
 # mat = bulkseq_mat # count data 
 population_labels = c("TBAC" = "A\u03b4-LTMR + A\u03b2-RA-LTMR",
                       "CRTH" = "C-LTMR",
@@ -20,26 +21,27 @@ sexlabels = c("F" = "Female", "M" = "Male")
 subpopulations = c(
   "Nociceptors 3D" = "TDNV_3D.csv",
   "Nociceptors 4W" = "TDNV_4W.csv",
-  "PEP 4W" = "CGRT_4W.csv", 
   "PEP 3D" = "CGRT_3D.csv",
-  "NP 4W" = "MRTD_4W.csv",
+  "PEP 4W" = "CGRT_4W.csv", 
   "NP 3D" = "MRTD_3D.csv",
-  "C-LTMR 4W" = "CRTH_4W.csv",
+  "NP 4W" = "MRTD_4W.csv",
   "C-LTMR 3D" = "CRTH_3D.csv",
-  "Ad- AB-RA LTMRs 4W" = "TBAC_4W.csv",
-  "Ad- AB-RA LTMRs 3D" = "TBAC_3D.csv"
+  "C-LTMR 4W" = "CRTH_4W.csv",
+  "Ad- AB-RA LTMRs 3D" = "TBAC_3D.csv",
+  "Ad- AB-RA LTMRs 4W" = "TBAC_4W.csv"
 )
+
 subpopulation_labels = c(
   "TDNV_3D" = "Nociceptors 3D",
   "TDNV_4W" = "Nociceptors 4W",
-  "CGRT_4W" = "PEP 4W", 
   "CGRT_3D" = "PEP 3D",
-  "MRTD_4W" = "NP 4W",
+  "CGRT_4W" = "PEP 4W", 
   "MRTD_3D" = "NP 3D",
-  "CRTH_4W" = "C-LTMR 4W",
+  "MRTD_4W" = "NP 4W",
   "CRTH_3D" = "C-LTMR 3D",
-  "TBAC_4W" = "Ad- AB-RA LTMRs 4W",
-  "TBAC_3D" = "Ad- AB-RA LTMRs 3D"
+  "CRTH_4W" = "C-LTMR 4W",
+  "TBAC_3D" = "Ad- AB-RA LTMRs 3D",
+  "TBAC_4W" = "Ad- AB-RA LTMRs 4W"
 )
 
 # a function that parse data into different dataframes based on the graph type 
@@ -59,17 +61,16 @@ plotline_server <- function(id, df, sex) {
       tcounts_med <- df %>% dplyr::group_by(Condition, Timepoint, symbol) %>% 
         dplyr::summarise(expression=median(expression))
       output$bulkseq_lines <- renderPlotly({
-        ggplot(data=tcounts_med, aes(x=Condition, y=expression, group=interaction(symbol, Timepoint)) ) + 
+        ggplot(data=tcounts_med, aes(x=Condition, y=expression, group=interaction(symbol, Timepoint))) + 
           scale_colour_viridis(discrete=TRUE, end = .80) + 
-          geom_line(aes(col=symbol, linetype=Timepoint)) + geom_point(aes(col=symbol)) + theme_bw() + 
-          theme(
-            panel.grid = element_blank(),
+          geom_line(aes(color=symbol, linetype=Timepoint)) + geom_point(aes(col=symbol)) + theme_bw() + 
+          theme(panel.grid = element_blank(),
             axis.title = element_text(family = "Gadugi", size=12),
             axis.text.x = element_text(family = "Gadugi", size=10, angle = 45, hjust= 1),
             axis.text.y = element_text(family = "Gadugi", size=10),
             axis.ticks.x = element_blank(),
-            axis.ticks.y = element_blank()) + ylab("Expression")+
-          labs(col="")
+            axis.ticks.y = element_blank(), legend.justification = c(0,0.3)) + ylab("Expression") +
+          guides(col=FALSE, linetype=guide_legend(label.hjust=0.5, ncol=8)) 
       })
     }
     
@@ -98,10 +99,10 @@ plotline_server <- function(id, df, sex) {
 # a server module for ploting dot plots 
 plotdot_server <- function(id, df, sex) {
   moduleServer(id, function(input, output, session) {
-    tcounts_med <- df %>% dplyr::group_by(Condition, Population, symbol, Sex) %>% 
-      dplyr::summarise(expression=median(expression))
-    tcounts_med <- tcounts_med[!tcounts_med$Condition %in% "Ipsi", ] #remove all injured samples
     if (sex == 'Both') {
+      tcounts_med <- df %>% dplyr::group_by(Condition, Population, symbol) %>% 
+        dplyr::summarise(expression=median(expression))
+      tcounts_med <- tcounts_med[!tcounts_med$Condition %in% "Ipsi", ] #remove all injured samples
       output$bulkseq_dots <- renderPlotly({
         ggplot(data = tcounts_med, aes(x= Population, y = symbol)) + 
           scale_colour_viridis_c(option = "magma", end = .90) + 
@@ -115,6 +116,9 @@ plotdot_server <- function(id, df, sex) {
       })
     }
     if (sex == "Separate") {
+      tcounts_med <- df %>% dplyr::group_by(Condition, Population, symbol, Sex) %>% 
+        dplyr::summarise(expression=median(expression))
+      tcounts_med <- tcounts_med[!tcounts_med$Condition %in% "Ipsi", ] #remove all injured samples
       output$bulkseq_dots <- renderPlotly({
         ggplot(data = tcounts_med, aes(x= Population, y = symbol)) + 
           scale_colour_viridis_c(option = "magma", end = .90) + 
@@ -223,6 +227,113 @@ deg_plot_server <- function(id, df) {
           axis.ticks.y = element_blank()) + 
         labs(shape = "", size = "") + scale_x_discrete(labels=subpopulation_labels)
     })
+
+  })
+}
+
+# functions for ploting volcano plots + highlighting selected genes
+volcano_plot <- function(df, ils) {
+  
+  il_genes <- df %>% filter(symbol %in% ils) 
+  
+  ggplot(data = df, aes(x = LFC, y = log10fdr)) + 
+    geom_point(colour = "grey", alpha = 0.5) +
+    geom_point(data = il_genes, # New layer containing data subset il_genes       
+               size = 2,
+               shape = 21,
+               fill = "firebrick",
+               colour = "black") + 
+    geom_hline(yintercept = -log10(0.05),
+               linetype = "dashed") + 
+    geom_vline(xintercept = c(log2(0.5), log2(2)),
+               linetype = "dashed") +
+    geom_label_repel(data = il_genes,   
+                     aes(label = symbol),
+                     force = 2,
+                     nudge_y = 1) + 
+    scale_colour_manual(values = cols) + 
+    scale_x_continuous(breaks = c(seq(-10, 10, 2)),     
+                       limits = c(-10, 10)) + theme_bw() + theme(
+                         panel.grid = element_blank(),
+                         axis.ticks.x = element_blank(),
+                         axis.ticks.y = element_blank(), 
+                         axis.text.x = element_text(size=12)) +
+    labs(y="-log10(FDR)")
+}
+
+volcano_plot_server <- function(id, ils) {
+  moduleServer(id, function(input, output, session){
+    
+    output$volcano1 <- renderPlot({
+      res = fread("TDNV_3D.csv")
+      colnames(res) <- c('symbol', 'LFC', 'FDR', 'ID')
+      mutated_ress = mutate(res, log10fdr=-log10(FDR))
+      volcano_plot(mutated_ress, ils)
+    })
+    
+    output$volcano2 <- renderPlot({
+      res = fread("TDNV_4W.csv")
+      colnames(res) <- c('symbol', 'LFC', 'FDR', 'ID')
+      mutated_ress = mutate(res, log10fdr=-log10(FDR))
+      volcano_plot(mutated_ress, ils)
+    })
+    
+    output$volcano3 <- renderPlot({
+      res = fread("CGRT_3D.csv")
+      colnames(res) <- c('symbol', 'LFC', 'FDR', 'ID')
+      mutated_ress = mutate(res, log10fdr=-log10(FDR))
+      volcano_plot(mutated_ress, ils)
+    })
+    
+    output$volcano4 <- renderPlot({
+      res = fread("CGRT_4W.csv")
+      colnames(res) <- c('symbol', 'LFC', 'FDR', 'ID')
+      mutated_ress = mutate(res, log10fdr=-log10(FDR))
+      volcano_plot(mutated_ress, ils)
+    })
+    
+    output$volcano5 <- renderPlot({
+      res = fread("MRTD_3D.csv")
+      colnames(res) <- c('symbol', 'LFC', 'FDR', 'ID')
+      mutated_ress = mutate(res, log10fdr=-log10(FDR))
+      volcano_plot(mutated_ress, ils)
+    })
+    
+    output$volcano6 <- renderPlot({
+      res = fread("MRTD_4W.csv")
+      colnames(res) <- c('symbol', 'LFC', 'FDR', 'ID')
+      mutated_ress = mutate(res, log10fdr=-log10(FDR))
+      volcano_plot(mutated_ress, ils)
+    })
+    
+    output$volcano7 <- renderPlot({
+      res = fread("CRTH_3D.csv")
+      colnames(res) <- c('symbol', 'LFC', 'FDR', 'ID')
+      mutated_ress = mutate(res, log10fdr=-log10(FDR))
+      volcano_plot(mutated_ress, ils)
+    })
+    
+    output$volcano8 <- renderPlot({
+      res = fread("CGRT_4W.csv")
+      colnames(res) <- c('symbol', 'LFC', 'FDR', 'ID')
+      mutated_ress = mutate(res, log10fdr=-log10(FDR))
+      volcano_plot(mutated_ress, ils)
+    })
+    
+    output$volcano9 <- renderPlot({
+      res = fread("TBAC_3D.csv")
+      colnames(res) <- c('symbol', 'LFC', 'FDR', 'ID')
+      mutated_ress = mutate(res, log10fdr=-log10(FDR))
+      volcano_plot(mutated_ress, ils)
+    })
+    
+    output$volcano10 <- renderPlot({
+      res = fread("TBAC_4W.csv")
+      colnames(res) <- c('symbol', 'LFC', 'FDR', 'ID')
+      mutated_ress = mutate(res, log10fdr=-log10(FDR))
+      volcano_plot(mutated_ress, ils)
+    })
+    
   })
 }
 
@@ -263,19 +374,6 @@ shinyServer(function(input, output, session) {
       updateTabItems(session, "tabs", newvalue)
     })
     
-    #output$genesearch <- eventReactive(input$goButton,{input$genequery})
-
-    # data <- reactive({
-    #     req(input$user_file)
-    # 
-    #     goi <- read.table(input$user_file$datapath)
-    #     rownames(goi) <- goi[,1]
-    # 
-    #     goi <- goi[which(rownames(goi) %in% rownames(bulkseq_mat)==TRUE),]
-    # 
-    #     return(goi)
-    # })
-    
     # data preprocessing for deg plots 
     deg_df <- data.frame()
     for (pop in subpopulations) {
@@ -285,7 +383,13 @@ shinyServer(function(input, output, session) {
       deg_df = bind_rows(deg_df, res)
     }
     mutateddf = mutate(deg_df, sig=ifelse(deg_df$FDR<0.05, "SIG", "NS"))
-
+   
+    # ploting volcano plots
+    observeEvent(input$volc, {
+      # plot volcano plots only when clicking the button
+      volcano_plot_server("volcano", input$geneid)
+    })
+    
     # for updating selected genes and plotting graphs, only after clicking 'plot graphs'
     observeEvent(input$load, {
       data <- reactive({
@@ -302,8 +406,8 @@ shinyServer(function(input, output, session) {
       }) %>% bindCache(input$geneid)
       
       deg_plot_server("deg_plot", reactive({deg()}))
-      
       goi_table_server("goi_table", reactive({data()}))
+      
     })
     
     # display table in 'hypothesis testing' page 
